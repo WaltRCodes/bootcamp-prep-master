0d70a3d4ef9eea54f054d50b27f94ee0c061f9a7
Walter <4wrada@gmail.com>
2019-11-23 11:45:55 -0500

    Update odd-couple.js

--- workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,7 +1,7 @@
 // YOUR CODE BELOW
 function oddCouple(entries) {
     let newEntries = [];
-    let count=0;
+    let count = 0;
     for (let i = 0; i < entries.length; i++){
         if (entries[i] % 2 != 0) {
             newEntries.push(entries[i])
